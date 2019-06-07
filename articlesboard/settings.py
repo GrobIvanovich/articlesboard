458b1b51f0e0a74fb51c5fd7b15678d867d13061
Dmitry <killmaster00431@gmail.com>
2019-06-07 16:20:56 +0500

    Test new MEDIA_URL

--- articlesboard/settings.py
+++ articlesboard/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://articlesboard.herokuapp.com/media/'
 
 # SITE_ID = 1
 
