5af6b123561993c314352d642c1f26e7a55db079
Dmitry <killmaster00431@gmail.com>
2019-06-11 13:02:44 +0500

    Debug set Off. Added companies app.

--- articlesboard/settings.py
+++ articlesboard/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i+j!kxzy0=v1k@8g)8hl(%w=f&7d!glt_5t_e4w6z@&sbo4^6w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'articlesboard.herokuapp.com', '0.0.0.0']
 
