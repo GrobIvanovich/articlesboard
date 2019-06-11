24f129be14055e1bdc794e3c9f13c6c02ceb90cd
Dmitry <killmaster00431@gmail.com>
2019-06-11 13:07:16 +0500

    Added remote site to db.

--- articlesboard/settings.py
+++ articlesboard/settings.py
@@ -132,8 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'https://articlesboard.herokuapp.com/media/'
 
-# SITE_ID = 1
-
 SITE_NAME = 'ArticlesSite'
 
 TAGGING_AUTOCOMPLETE_SEARCH_CONTAINS = True
