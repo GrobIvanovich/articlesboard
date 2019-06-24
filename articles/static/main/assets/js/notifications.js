bc276d34ad28a1a418cb52ad001cacf568dae0f6
GrobIvanovich <killmaster00431@gmail.com>
2019-06-24 11:57:03 +0500

    Added markdown field to add_article page. Created preview. Working on #76.

--- articles/static/main/assets/js/notifications.js
+++ articles/static/main/assets/js/notifications.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
             count++;
         });
     }).done(function() {
-	    console.log('Notifications: ' + count);
+	    // console.log('Notifications: ' + count);
 	    if (count > 0){
 	    	$('.notification').html(count);
 		}
